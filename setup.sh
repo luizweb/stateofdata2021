20892a471646daf78cf9b5bddb8ab97f4a24271e
Luiz Simões <57963216+luizweb@users.noreply.github.com>
2022-06-29 17:26:24 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 mkdir -p ~/.streamlit/
 echo "
-[general]\n
-email = "luizgsantonio@gmail.com"\n
-" > ~/.streamlit/credentials.toml
-echo "
 [server]\n
 headless = true\n
 enableCORS=false\n
 port = $PORT\n
+\n
 " > ~/.streamlit/config.toml
